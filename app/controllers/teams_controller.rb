680a139e210fb17796a0f718b4f4691910933de1
Alden Peeters <starbounder1024@gmail.com>
2018-03-05 19:01:42 -0500

    Did matches views & stuff

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,6 +15,7 @@ class TeamsController < ApplicationController
 			format.html {
 				@comments = Comment.where(team_key: @team.key);
 				@pictures = Picture.where(team_key: @team.key);
+				@matches = Match.find_by_sql("SELECT * FROM matches WHERE red1='" + @team.number.to_s + "' OR red2='" + @team.number.to_s + "' OR red3='" + @team.number.to_s + "' OR blue1='" + @team.number.to_s + "' OR blue2='" + @team.number.to_s + "' OR blue3='" + @team.number.to_s + "'");
 			}
 			format.json {
 				render :json => @team, :except => [:created_at, :updated_at], :include => [{:comments => {:only => :body}}, {:pictures => {:only => [:picture_type, :taken_at, :link]}}]
