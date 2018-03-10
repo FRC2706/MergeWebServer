ce875631e70f895f180c476b91657bbc0ae5e236
Alden Peeters <starbounder1024@gmail.com>
2018-03-10 17:49:58 -0500

    Finished competitions list/show
    This should pretty much clear up #8, although there's a couple things left to do

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,11 +1,23 @@
 class CompetitionsController < ApplicationController
 	def list
 		@competitions = Competition.all
-		render :json => @competitions, :only => [:key, :name]
+		respond_to do |format|
+			format.html
+			format.json {
+				render :json => @competitions, :only => [:key, :name]
+			}
+		end
 	end
 	
 	def show
 		@competition = Competition.find_by(key: params[:competition]);
-		render :json => @competition, :except => [:created_at, :updated_at], :include => {:matches => {:only => [:match_number, :key]}}
+		respond_to do |format|
+			format.html {
+				@matches = Match.where(:competition_key => @competition.key);
+			}
+			format.json {
+				render :json => @competition, :except => [:created_at, :updated_at], :include => {:matches => {:only => [:match_number, :key]}}
+			}
+		end
 	end
 end
